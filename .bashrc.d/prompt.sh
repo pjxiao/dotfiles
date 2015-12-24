c8fecc82ef1f9d96069d3deebc319ee4f77d14e9
Takayuki Hirai <namidame.xiao.xing.mell.1.0.1x@gmail.com>
2015-12-24 14:56:29 +0900

    Show Git branch on PS1

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,3 +1,17 @@
-[ -n "$PS1" ] &&\
+
+
+if [ -n "$PS1" ]
+then
     ps --no-headers | awk '{ print $4 }' | grep -q vim &&\
-    PS1="(\[\e[;32m\]VIM\[\e[00m\])"$PS1
+        PS1="(\[\e[;32m\]VIM\[\e[00m\])"$PS1
+
+    if $(grep -qE '\s__git_ps1$' <(declare -F))
+    then
+        __git_branch ()
+        {
+            __git_ps1 | sed -e 's/^\s\+//'
+        }
+       PS1='$(__git_branch)'$PS1
+    fi
+fi
+
