1cafe9fcfc57eaeca798c30cfabaead84c3dc91f
Takayuki Hirai <namidame.xiao.xing.mell.1.0.1x@gmail.com>
2015-12-25 16:22:57 +0900

    Add over-VCS command shortcuts

--- .bashrc.d/function.sh
+++ .bashrc.d/function.sh
@@ -8,3 +8,39 @@ respawn() {
         echo "Restarting..."
     done
 }
+
+
+__repo_diff()
+{
+    if [ -n "$(__git_branch)" ]; then
+        echo 'Git'
+        git diff
+        return
+    fi
+
+    if [ -n "$(__hg_branch)" ]; then
+        echo 'Mercurial'
+        hg diff
+        return
+    fi
+}
+
+
+__repo_st()
+{
+    if [ -n "$(__git_branch)" ]; then
+        echo 'Git'
+        git status
+        return
+    fi
+
+    if [ -n "$(__hg_branch)" ]; then
+        echo 'Mercurial'
+        hg status
+        return
+    fi
+}
+
+
+alias rst=__repo_st
+alias rdf=__repo_diff
