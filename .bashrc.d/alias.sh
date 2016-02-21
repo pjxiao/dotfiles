f57d5e9f70d9105b5b6c1a2f60aea50df07b2f0e
Takayuki Hirai <namidame.xiao.xing@gmail.com>
2016-02-21 23:41:41 +0900

    alias as local binary

--- .bashrc.d/alias.sh
+++ .bashrc.d/alias.sh
@@ -1,3 +1,23 @@
+# Prepare
+bin_dir=$(mktemp -d)
+
+__alias ()
+{
+    local cmd="$(echo "$1" | cut -d= -f1)"
+    local script="$(echo "$1" | cut -d= -f2-)"
+
+    cat <<EOL > "${bin_dir}/${cmd}"
+#!/bin/bash
+export PATH=${PATH}
+${script}
+EOL
+    chmod +x "${bin_dir}/${cmd}"
+}
+
+alias alias="__alias"
+
+
+# misc
 alias docker='sudo docker'
 alias dl='docker ps -l -q'
 alias l='ls -laF --color'
@@ -6,22 +26,36 @@ alias mv='mv -vi'
 alias cp='cp -vi'
 alias diff='colordiff -u'
 
-alias wget='wget --content-disposition --max-redirect 10'
+# Network
+alias wget='wget --content-disposition --max-redirect 10 -c -t0'
 alias curl='curl -c ~/.curlcookie -b ~/.curlcookie -L'
 
 # Shortcut
-alias yesterday='date --date "1 day ago" +"%Y-%m-%d"'
-alias yst='yesterday'
+__yesterday='date --date "1 day ago" +"%Y-%m-%d"'
+alias yesterday="${__yesterday}"
+alias yst="${__yesterday}"
+unset __yesterday
 alias open-tunnel='start-stop-daemon --start --background --make-pidfile --pidfile /var/run/sshtunnel.pid --exec /usr/local/bin/sshtunnel -- '
 alias close-tunnel='start-stop-daemon --stop --pidfile /var/run/sshtunnel.pid'
 alias pingle='ping google.com'
-alias clipboard='xsel --clipboard --input'
+
+which xsel >/dev/null 2>&1   && alias clipboard='xsel --clipboard --input'
+which rlwrap >/dev/null 2>&1 && alias gosh='rlwrap gosh'
 
 # Git
-git_status='git status -bs'
-alias gstat="${git_status}"
-alias gst="${git_status}"
+__git_status='git status -bs'
+alias gstat="${__git_status}"
+alias gst="${__git_status}"
+unset __git_status
 alias gdf='git diff'
 alias gcm='git commit -m'
 alias glg="git log --stat"
 alias glgr="git log --graph --date-order --pretty=format:'%Cblue%h %Cgreen%ci %Cred%an %Cblue%m %Creset%s %Cred%d'"
+
+
+# Setup & Clean-up
+unalias alias
+rm -rf "${HOME}/.local/bin"
+mv "${bin_dir}" "${HOME}/.local/bin"
+export PATH="${HOME}/.local/bin":$PATH
+unset bin_dir
